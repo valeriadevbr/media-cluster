d8b3f6683c169f1c96cc06a692531aac925ca9df
Valéria Acciaroli <valeria.acciaroli@livup.com.br>
2025-12-27 23:54:11 -0300

    websecure-wan não acessa as apps internas

--- setup/k8s/bootstrap/05-traefik.sh
+++ setup/k8s/bootstrap/05-traefik.sh
@@ -9,21 +9,26 @@ helm repo update traefik >/dev/null
 helm upgrade --install traefik traefik/traefik \
   --namespace ingress-traefik \
   --create-namespace \
-  --set ports.websecure.http3.enabled=true \
-  --set providers.kubernetesIngress.enabled=true \
-  --set service.type=NodePort \
+  --set "ports.websecure.http3.enabled=true" \
+  --set "providers.kubernetesIngress.enabled=true" \
+  --set "service.type=NodePort" \
   --set "additionalArguments={--serverstransport.insecureskipverify=true}" \
   --set "ports.web.middlewares[0]=ingress-traefik-gzip@kubernetescrd" \
-  --set ports.web.nodePort=80 \
+  --set "ports.web.nodePort=80" \
+  --set "ports.web-wan.middlewares[0]=ingress-traefik-gzip@kubernetescrd" \
+  --set "ports.web-wan.port=44000" \
+  --set "ports.web-wan.exposedPort=44000" \
+  --set "ports.web-wan.nodePort=44000" \
   --set "ports.websecure.middlewares[0]=ingress-traefik-gzip@kubernetescrd" \
-  --set ports.websecure.nodePort=443 \
-  --set "ports.websecure-alt.middlewares[0]=ingress-traefik-gzip@kubernetescrd" \
-  --set ports.websecure-alt.port=44300 \
-  --set ports.websecure-alt.exposedPort=44300 \
-  --set ports.websecure-alt.nodePort=44300 \
-  --set deployment.podSecurityContext.runAsNonRoot=true \
-  --set deployment.podSecurityContext.runAsUser=65532 \
-  --set deployment.podSecurityContext.runAsGroup=65532 \
+  --set "ports.websecure.nodePort=443" \
+  --set "ports.websecure-wan.middlewares[0]=ingress-traefik-gzip@kubernetescrd" \
+  --set "ports.websecure-wan.port=44300" \
+  --set "ports.websecure-wan.exposedPort=44300" \
+  --set "ports.websecure-wan.nodePort=44300" \
+  --set "ports.websecure-wan.tls.enabled=true" \
+  --set "deployment.podSecurityContext.runAsNonRoot=true" \
+  --set "deployment.podSecurityContext.runAsUser=65532" \
+  --set "deployment.podSecurityContext.runAsGroup=65532" \
   --set "deployment.securityContext.capabilities.add={NET_BIND_SERVICE}" \
   --wait >/dev/null
 
