e7bc0a3465618684ce5e5b9619d317058e34b764
Valéria Acciaroli <valeria.acciaroli@livup.com.br>
2025-12-30 20:40:09 -0300

    traefik dashboard

--- setup/k8s/bootstrap/05-traefik.sh
+++ setup/k8s/bootstrap/05-traefik.sh
@@ -7,32 +7,9 @@ helm repo add traefik https://traefik.github.io/charts >/dev/null
 helm repo update traefik >/dev/null
 
 helm upgrade --install traefik traefik/traefik \
-  --namespace ingress-traefik \
   --create-namespace \
-  --set "providers.kubernetesIngress.enabled=true" \
-  --set "service.type=NodePort" \
-  --set "additionalArguments={--serverstransport.insecureskipverify=true}" \
-  --set "ports.web.middlewares[0]=ingress-traefik-gzip@kubernetescrd" \
-  --set "ports.web.nodePort=80" \
-  --set "ports.web-wan.expose.default=true" \
-  --set "ports.web-wan.middlewares[0]=ingress-traefik-gzip@kubernetescrd" \
-  --set "ports.web-wan.port=44000" \
-  --set "ports.web-wan.exposedPort=44000" \
-  --set "ports.web-wan.nodePort=44000" \
-  --set "ports.websecure.http3.enabled=true" \
-  --set "ports.websecure.middlewares[0]=ingress-traefik-gzip@kubernetescrd" \
-  --set "ports.websecure.nodePort=443" \
-  --set "ports.websecure-wan.expose.default=true" \
-  --set "ports.websecure-wan.http3.enabled=true" \
-  --set "ports.websecure-wan.middlewares[0]=ingress-traefik-gzip@kubernetescrd" \
-  --set "ports.websecure-wan.port=44300" \
-  --set "ports.websecure-wan.exposedPort=44300" \
-  --set "ports.websecure-wan.nodePort=44300" \
-  --set "ports.websecure-wan.tls.enabled=true" \
-  --set "deployment.podSecurityContext.runAsNonRoot=true" \
-  --set "deployment.podSecurityContext.runAsUser=65532" \
-  --set "deployment.podSecurityContext.runAsGroup=65532" \
-  --set "deployment.securityContext.capabilities.add={NET_BIND_SERVICE}" \
+  --namespace ingress-traefik \
+  -f "$(dirname "$0")/resources/traefik-values.yaml" \
   --wait >/dev/null
 
 echo "Aguardando Traefik..."
